e8bc3b1c9c13f9577e8c778532a5488234ac89b3
nesmy <pnesmy@gmail.com>
2022-12-09 07:59:32 -0500

    first commit

---
+++ build/CMakeFiles/Zoe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Zoe.dir/src/main.cpp.obj"
+  "CMakeFiles/Zoe.dir/src/main.cpp.obj.d"
+  "Zoe.exe"
+  "Zoe.exe.manifest"
+  "Zoe.pdb"
+  "libZoe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Zoe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Zoe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Zoe.
+# This may be replaced when dependencies are built.
